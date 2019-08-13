e43544733e64fa81612d49e87af6813df242a21a
Onengiye Richard <klevamane@gmail.com>
2019-08-13 09:40:41 +0100

    add postgres db and update auth_user_model to user.User

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restframework',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'airtech',
+        'USER': 'user',
+        'PASSWORD': 'user',
+        'PORT': 5432
     }
 }
 
@@ -118,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# tell django which model to use as user model
+AUTH_USER_MODEL = 'user.User'
--- user/models.py
+++ user/models.py
@@ -1,4 +1,69 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
+
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, lastname, firstname, date_of_birth, **extrafields):
+        if not email:
+            ValueError ('Email field is required')
+        if not firstname:
+            ValueError ('Firstname field is required')
+        if not lastname:
+            ValueError ('Lastname field is required')
+        if not date_of_birth:
+            ValueError ('Date of birth field is required')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            password=password,
+            firstname=firstname,
+            lastname=lastname,
+            date_of_birth=date_of_birth,
+            **extrafields
+        )
+
+        user.is_active = True,
+        user.set_password(password),
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password, lastname, firstname, date_of_birth):
+        user = self.create_user (email, password, lastname, firstname, date_of_birth)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    object = UserManager()
+
+    REQUIRED_FIELDS = ['firstname', 'lastname', 'date_of_birth']
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return '{} {}'.format(self.firstname, self.lastname)
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_username(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
